4bf149a739ab52bf93aeb9208146fa658d9aafe9
Alejandro <alexnachovc12@gmail.com>
2024-02-09 23:54:36 -0600

    Arithmetic Operators

---
+++ Arithmetic_Operators/index.js
@@ -0,0 +1,40 @@
+// Arithmetic operators
+
+let students = 30;
+
+/*
+Operands and Operators 
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 2;
+let extraStudents = students % 3;
+*/
+
+
+/*
+Augmented Assignment Operators 
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students %= 2;
+*/
+
+
+/*
+Increment and Decrement operators
+
+students++;
+students--;
+*/
+
+
+/* Operator precedence uses PEMDAS
+let result = 6 / 2 ** (2 + 5);
+*/
+
+console.log(result);
