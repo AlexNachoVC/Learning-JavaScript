540032b371b96c6523ba024ab2eaed883ded98cd
Alejandro <alexnachovc12@gmail.com>
2024-02-28 12:38:58 -0600

    String Methods

--- String_Methods/index.js
+++ String_Methods/index.js
@@ -2,4 +2,75 @@
 
 let username = "AlexNachoVC";
 
-console.log(username.charAt(3));
+//console.log(username.charAt(3)); 
+
+//console.log(username.length)
+
+/*
+username = username.trim();
+console.log(username);
+*/
+
+/*
+username = username.toLowerCase();
+console.log(username);
+*/
+
+/*
+username = username.repeat(3);
+console.log(username);
+*/
+
+/*
+let result = username.startsWith(" ");
+
+if (result) {
+    console.log("Your username can't begin with ' '");
+} else {
+    console.log(username);
+}
+*/
+
+/*
+let result = username.endsWith(" ");
+
+if (result) {
+    console.log("Your username can't end with ' '");
+} else {
+    console.log(username);
+}
+*/
+
+/*
+let result = username.includes(" ");
+
+if (result) {
+    console.log("Your username can't include ' '");
+} else {
+    console.log(username);
+}
+*/
+
+/*
+let phoneNumber = "123-456-789";
+
+phoneNumber = phoneNumber.replaceAll("-","/");
+
+console.log(phoneNumber);
+
+*/
+/*
+let phoneNumber = "123-456-789";
+
+phoneNumber = phoneNumber.padStart(15, "0");
+
+console.log(phoneNumber);
+*/
+
+
+let phoneNumber = "123-456-789";
+
+phoneNumber = phoneNumber.padEnd(15, "0");
+
+console.log(phoneNumber);
+
