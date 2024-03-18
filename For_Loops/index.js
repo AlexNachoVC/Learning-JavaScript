7a73247354ef9695cd0e134b385a14614af9ba3e
Alejandro <alexnachovc12@gmail.com>
2024-03-17 20:45:43 -0600

    For Loops

--- For_Loops/index.js
+++ For_Loops/index.js
@@ -1,1 +1,23 @@
 // For Loop = repeat some code a LIMITED amount of times
+
+/*
+for (let i = 0; i < 10; i++ ) {
+    console.log(i);
+} 
+
+for (let i = 10; i > 0; i-- ) {
+    console.log(i);
+} 
+
+console.log("Happy New Year!")
+*/
+
+for (let i = 1; i <= 20; i++) {
+    
+    if (i == 13) {
+        continue;
+    } else {
+        console.log(i);
+    }
+
+}
