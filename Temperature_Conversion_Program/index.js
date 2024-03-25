3fd99398df68f3215249b5b76f33bc9c6ae86b72
Alejandro <alexnachovc12@gmail.com>
2024-03-24 23:38:26 -0600

    Temperature Conversion Program

--- Temperature_Conversion_Program/index.js
+++ Temperature_Conversion_Program/index.js
@@ -1,1 +1,21 @@
 // Temperature Conversion Program
+
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+    if (toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = (temp * 1.8) + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    } else if (toCelsius.checked) {
+        temp = Number(textBox.value);
+        temp = 1.8 * (temp - 32);
+        result.textContent = temp.toFixed(1) + "°C";
+    } else {
+        result.textContent = "Select a unit"
+    }
+}
